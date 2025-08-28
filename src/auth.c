7b7326e0d139aacaf17fed2426893a97a4d40bda
mshaban <maryam.redhha932@gmail.com>
2025-08-28 16:40:50 +0300

    add

--- src/auth.c
+++ src/auth.c
@@ -88,9 +88,12 @@ void user_menu(int user_id, char* user_name) {
 	int choice;
 	while (1) {
 		printf("\n==== User Menu ====\n");
-		printf("1. Create Account\n");
-		printf("2. List Accounts\n");
-		printf("3. Logout\n");
+	printf("1. Create Account\n");
+	printf("2. List Accounts\n");
+	printf("3. Update Account Info\n");
+	printf("4. Check Account Details\n");
+	printf("5. Make Transaction\n");
+	printf("6. Logout\n");
 		printf("Enter your choice: ");
 		scanf("%d", &choice);
 		getchar();
@@ -121,7 +124,160 @@ void user_menu(int user_id, char* user_name) {
 				}
 				break;
 			}
-			case 3:
+			case 3: {
+				int update_acc_id, found = 0;
+				char field[10], new_value[COUNTRY_LEN > PHONE_LEN ? COUNTRY_LEN : PHONE_LEN];
+				printf("Enter the account ID to update: ");
+				scanf("%d", &update_acc_id); getchar();
+				printf("Which field do you want to update? (country/phone): ");
+				fgets(field, sizeof(field), stdin);
+				field[strcspn(field, "\n")] = 0;
+				if (strcmp(field, "country") != 0 && strcmp(field, "phone") != 0) {
+					printf("Invalid field. Only 'country' or 'phone' can be updated.\n");
+					break;
+				}
+				printf("Enter new value: ");
+				fgets(new_value, sizeof(new_value), stdin);
+				new_value[strcspn(new_value, "\n")] = 0;
+
+				FILE *fp = fopen("../data/records.txt", "r");
+				if (!fp) fp = fopen("data/records.txt", "r");
+				FILE *tmp = fopen("../data/records_tmp.txt", "w");
+				if (!tmp) tmp = fopen("data/records_tmp.txt", "w");
+				int rec_id, uid, acc_id;
+				char uname[NAME_LEN], date[11], country[COUNTRY_LEN], phone[PHONE_LEN], type[TYPE_LEN];
+				double balance;
+				if (fp && tmp) {
+					while (fscanf(fp, "%d %d %s %d %10s %s %s %lf %s", &rec_id, &uid, uname, &acc_id, date, country, phone, &balance, type) == 9) {
+						if (uid == user_id && acc_id == update_acc_id) {
+							found = 1;
+							if (strcmp(field, "country") == 0) {
+								strncpy(country, new_value, COUNTRY_LEN);
+							} else if (strcmp(field, "phone") == 0) {
+								strncpy(phone, new_value, PHONE_LEN);
+							}
+						}
+						fprintf(tmp, "%d %d %s %d %s %s %s %.2lf %s\n", rec_id, uid, uname, acc_id, date, country, phone, balance, type);
+					}
+					fclose(fp);
+					fclose(tmp);
+					// Replace original file
+					remove("../data/records.txt");
+					remove("data/records.txt");
+					rename("../data/records_tmp.txt", "../data/records.txt");
+					rename("data/records_tmp.txt", "data/records.txt");
+					if (found) {
+						printf("Account updated successfully!\n");
+					} else {
+						printf("Account not found or you do not have permission to update it.\n");
+					}
+				} else {
+					printf("Error updating account.\n");
+				}
+				break;
+			}
+			case 4: {
+				int check_acc_id, found = 0;
+				printf("Enter the account ID to view: ");
+				scanf("%d", &check_acc_id); getchar();
+				FILE *fp = fopen("../data/records.txt", "r");
+				if (!fp) fp = fopen("data/records.txt", "r");
+				int rec_id, uid, acc_id;
+				char uname[NAME_LEN], date[11], country[COUNTRY_LEN], phone[PHONE_LEN], type[TYPE_LEN];
+				double balance;
+				if (fp) {
+					while (fscanf(fp, "%d %d %s %d %10s %s %s %lf %s", &rec_id, &uid, uname, &acc_id, date, country, phone, &balance, type) == 9) {
+						if (uid == user_id && acc_id == check_acc_id) {
+							found = 1;
+							printf("\nAccount Details:\n");
+							printf("Record ID: %d\nAccount ID: %d\nUser: %s\nDate: %s\nCountry: %s\nPhone: %s\nBalance: %.2lf\nType: %s\n", rec_id, acc_id, uname, date, country, phone, balance, type);
+							// Interest calculation
+							double interest = 0.0;
+							if (strcmp(type, "savings") == 0) {
+								interest = balance * 0.07 / 12;
+								printf("You will get $%.2lf as interest on day %c%c of every month.\n", interest, date[0], date[1]);
+							} else if (strcmp(type, "fixed01") == 0) {
+								interest = balance * 0.04 / 12;
+								printf("You will get $%.2lf as interest on day %c%c of every month.\n", interest, date[0], date[1]);
+							} else if (strcmp(type, "fixed02") == 0) {
+								interest = balance * 0.05 / 12;
+								printf("You will get $%.2lf as interest on day %c%c of every month.\n", interest, date[0], date[1]);
+							} else if (strcmp(type, "fixed03") == 0) {
+								interest = balance * 0.08 / 12;
+								printf("You will get $%.2lf as interest on day %c%c of every month.\n", interest, date[0], date[1]);
+							} else if (strcmp(type, "current") == 0) {
+								printf("You will not get interests because the account is of type current.\n");
+							}
+							break;
+						}
+					}
+					fclose(fp);
+				}
+				if (!found) {
+					printf("Account not found or you do not have permission to view it.\n");
+				}
+				break;
+			}
+			case 5: {
+				int trans_acc_id, found = 0;
+				char trans_type[10];
+				double amount;
+				printf("Enter the account ID for transaction: ");
+				scanf("%d", &trans_acc_id); getchar();
+				printf("Enter transaction type (deposit/withdraw): ");
+				fgets(trans_type, sizeof(trans_type), stdin);
+				trans_type[strcspn(trans_type, "\n")] = 0;
+				printf("Enter amount: ");
+				scanf("%lf", &amount); getchar();
+
+				FILE *fp = fopen("../data/records.txt", "r");
+				if (!fp) fp = fopen("data/records.txt", "r");
+				FILE *tmp = fopen("../data/records_tmp.txt", "w");
+				if (!tmp) tmp = fopen("data/records_tmp.txt", "w");
+				int rec_id, uid, acc_id;
+				char uname[NAME_LEN], date[11], country[COUNTRY_LEN], phone[PHONE_LEN], type[TYPE_LEN];
+				double balance;
+				if (fp && tmp) {
+					while (fscanf(fp, "%d %d %s %d %10s %s %s %lf %s", &rec_id, &uid, uname, &acc_id, date, country, phone, &balance, type) == 9) {
+						if (uid == user_id && acc_id == trans_acc_id) {
+							found = 1;
+							if (strcmp(type, "fixed01") == 0 || strcmp(type, "fixed02") == 0 || strcmp(type, "fixed03") == 0) {
+								printf("Transactions are not allowed for fixed accounts.\n");
+								fprintf(tmp, "%d %d %s %d %s %s %s %.2lf %s\n", rec_id, uid, uname, acc_id, date, country, phone, balance, type);
+								continue;
+							}
+							if (strcmp(trans_type, "deposit") == 0) {
+								balance += amount;
+								printf("Deposit successful. New balance: %.2lf\n", balance);
+							} else if (strcmp(trans_type, "withdraw") == 0) {
+								if (balance < amount) {
+									printf("Insufficient funds. Transaction cancelled.\n");
+								} else {
+									balance -= amount;
+									printf("Withdrawal successful. New balance: %.2lf\n", balance);
+								}
+							} else {
+								printf("Invalid transaction type.\n");
+							}
+						}
+						fprintf(tmp, "%d %d %s %d %s %s %s %.2lf %s\n", rec_id, uid, uname, acc_id, date, country, phone, balance, type);
+					}
+					fclose(fp);
+					fclose(tmp);
+					// Replace original file
+					remove("../data/records.txt");
+					remove("data/records.txt");
+					rename("../data/records_tmp.txt", "../data/records.txt");
+					rename("data/records_tmp.txt", "data/records.txt");
+					if (!found) {
+						printf("Account not found or you do not have permission to transact on it.\n");
+					}
+				} else {
+					printf("Error processing transaction.\n");
+				}
+				break;
+			}
+			case 6:
 				printf("Logging out...\n");
 				return;
 			default:
